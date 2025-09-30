3c16ea6d1ba4151979548a36be5b87bb426c2e71
Mario-Code7 <marioroa71@yahoo.com>
2025-09-30 16:00:35 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.print("Please enter a game score: ");
+        String input = myScanner.nextLine().trim();
+
+    }
+}
